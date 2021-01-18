e7a3e308f8ea390044e7e064fd54220f3b9b2638
RafaelSS <Rafaelss427@gmail.com>
2021-01-18 11:44:45 -0600

    Segundo commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import { saludo } from './js/componentes';
 import './styles.css';
 
 const nombre = 'Rafael 688989'; 
+
+const nombre1 = 'Rafael 688989'; 
+
+const nombre2 = 'Rafael 688989';
+ 
 saludo(nombre);
